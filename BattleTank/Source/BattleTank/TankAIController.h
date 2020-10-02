a122a1c649730f64a2ca89acfde07e0d0961218f
Gun_Smoke <gunsmoke9876@gmail.com>
2020-10-02 14:39:55 +0300

    Created basic TankPlayerController and TankAIController, that just reports the names of the tank they are belong to. Also, TankAIController reports the name of player's tank he found in the game.

---
+++ BattleTank/Source/BattleTank/TankAIController.h
@@ -0,0 +1,28 @@
+// © GunSmoke, 2020
+
+#pragma once
+
+#include "AIController.h"
+#include "CoreMinimal.h"
+#include "Tank.h"
+#include "TankAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankAIController : public AAIController
+{
+	GENERATED_BODY()
+
+	public:
+	ATank* GetControlledTank() const;
+
+	virtual void BeginPlay() override; //Postfix "override" makes sure, that 
+	                                   //there is a "virtual void BeginPlay()"
+									   //in some of the parent classes, and
+									   // you are defining an overriding
+									   // function, not new one.
+	ATank* GetPlayerTank() const;
+	
+};
