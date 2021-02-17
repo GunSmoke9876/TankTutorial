4f04b0f57a2bd44bdbc41ed78a85a281b6bc25c8
Gun_Smoke <gunsmoke9876@gmail.com>
2021-02-17 03:27:15 +0300

    Passed "Finding Screen Pixel Coordinates" lesson.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -21,10 +21,53 @@ void ATankPlayerController::BeginPlay()
     UE_LOG(LogTemp, Warning, TEXT("Tank Pawn Name is %s"),  *ControlledTank->GetName());
 }
 
+// Called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+    AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());    //Gets the pawn of player controller and 
                                       // casts it to ATank.
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+    if(!GetControlledTank())
+        return;
+    //Then ray-trace to find intersection aiming line with the landscape.
+    FVector AimingLocation;
+    GetSightRayAimLocation(&AimingLocation);
 
+    UE_LOG(LogTemp, Warning, TEXT("AimingLocation = %s"), *(AimingLocation.ToString()));
+}
+
+//Get world location of linetrace through crosshair. True, if line of sight has an intersection with the world.
+bool ATankPlayerController::GetSightRayAimLocation(FVector* AimLocation) const
+{
+    //get camera's coords
+
+    //get direction to crosshair: considering camera direction is screen center, 
+    //  calculate crosshair direction as camera direction + (crosshair_coords_from_center * angle_per_pixel)
+    //  angle_per_pixel is known by the screen size and current camera angle of view (FOV)
+
+    //line trace from camera coords in direction of crosshair
+    //return true, if the intersection exists, and false otherwise.
+
+
+    //acquire current screen size in pixels
+    int32 ViewportWidth, ViewportHeight;
+    GetViewportSize(ViewportWidth, ViewportHeight);
+
+    //Crosshair screen coords
+    auto CrosshairPosition = FVector2D(ViewportWidth * CrosshairX, ViewportHeight * CrosshairY);
+
+    //Get camera coords and direction
+
+    //For testing modify of the out parameter:
+    *AimLocation = FVector(1.0);
+    return false;
+}
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -23,4 +23,22 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 									   //in some of the parent classes, and
 									   // you are defining an overriding
 									   // function, not new one.
+									   // Called every frame
+	void Tick(float DeltaTime) override;
+
+
+	private:
+	//Makes tank to rotate it's barrel to crosshair's direction.
+	void AimTowardsCrosshair();
+
+	//Retuns coords of intersection line of sight with landscape.
+	bool GetSightRayAimLocation(FVector* AimLocation) const; 
+
+	//Screen coordinate X of crosshair, set up in TankPlayerController_BP blueprint (0..1).
+	UPROPERTY(EditAnywhere)
+	float CrosshairX = 0.5;
+	
+	//Screen coordinate Y of crosshair, set up in TankPlayerController_BP blueprint (0..1).
+	UPROPERTY(EditAnywhere)
+	float CrosshairY = 0.4;
 };
